77f5000e585be7a5b29c9464bc35240135a83a98
Rameen Mehmood <rameenmehmood3@gmail.com>
2024-08-29 20:00:55 +0500

    updated

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,99 +1,11 @@
-// import React from 'react'
-
-// const Projects = () => {
-//   return (
-//     <>
-//       <div className="container c-height">
-//         <h3 className='font-size text-center'>Projects</h3>
-//         <div className="cards ">
-//           <div className="card flex-card">
-//             <div >
-//               <img src="/vite.svg" alt="" />
-//             </div>
-//             <div className="">
-//               <h4> Blog Leaf </h4>
-//               <p>A Full-Featured Blog Posting Platform</p>
-//               <ul>
-//                 <li><strong>USER AUTHENTICATION:</strong> Create accounts and manage profiles with secure authentication and database management via Appwrite.</li>
-//                 <li><strong>RICH TEXT EDITOR:</strong> Enhanced content editing with TinyMCE API.</li>
-//                 <li><strong>FORM MANAGEMENT:</strong> Efficient and streamlined form handling using React Hook Form.</li>
-//               </ul>
-//               <div className="m-top">
-//                 <span className='b'>react</span>
-//                 <span className='b'>react</span>
-//                 <span className='b'>react</span>
-//               </div>
-//               <div className="flex">
-//                 <a href="https://your-blog-leaf-live-site.com"  target="_blank">Visit Website</a>
-//                 <a href="https://github.com/your-repository-link" target="_blank">Explore the Code</a>
-//               </div>
-//             </div>
-//           </div>
-//           <div className="card flex-card">
-//             <div >
-//               <img src="/vite.svg" alt="" />
-//             </div>
-//             <div className="">
-//               <h4> Spotify Clone </h4>
-//               <p>Responsive Design and JavaScript-Enabled Song Playback</p>
-//               <ul>
-//                 <li><strong> UI/UX Design & RESPONSIVE DESIGN:</strong> A sleek, user-friendly interface that adapts seamlessly to different screen sizes.</li>
-//                 <li><strong>INTERACTIVE JS FEATURES:</strong> JavaScript-powered song playback and intuitive navigation to enhance user experience.</li>
-//               </ul>
-//               <div className="m-top">
-//                 <span className='b'>react</span>
-//                 <span className='b'>react</span>
-//                 <span className='b'>react</span>
-//               </div>
-//               <div className="flex">
-//                 <a href="https://your-blog-leaf-live-site.com"  target="_blank">Visit Website</a>
-//                 <a href="https://github.com/your-repository-link" target="_blank">Explore the Code</a>
-//               </div>
-//             </div>
-//           </div>
-//           <div className="card flex-card">
-//             <div>
-//               <img src="/vite.svg" alt="" />
-//             </div>
-//             <div className="">
-//               <h4> Netflix Clone </h4>
-//               <p>A replica of Netflix’s cover page, showcasing attention to detail in layout and design, aimed at delivering an engaging user experience.</p>
-//               <ul>
-//                 <li><strong>UI/UX DESIGN:</strong> Creating visually appealing, user-friendly interfaces that enhance the overall user experience.</li>
-//                 <li><strong>RESPONSIVE DESIGN:</strong> Implementing responsive design principles to ensure compatibility across various devices.</li>
-//               </ul>
-//               <div className="m-top">
-//                 <span className='b'>react</span>
-//                 <span className='b'>react</span>
-//                 <span className='b'>react</span>
-//               </div>
-
-//               <div className="flex">
-//                 <a href="https://your-blog-leaf-live-site.com" target="_blank">Visit Website</a>
-//                 <a href="https://github.com/your-repository-link"  target="_blank">Explore the Code</a>
-//               </div>
-//             </div>
-//           </div>
-
-
-//         </div>
-//       </div>
-//     </>
-//   )
-// }
-
-// export default Projects
-
-
 import React from 'react';
-import Card from './Card'; // Make sure to update the import path according to your file structure
+import Card from './Card'; 
 import { motion } from "framer-motion"
 
 
 const Projects = () => {
   const projects = [
     {
-      // image:'/blog.jfif',
       video: '/blog-video.mp4',
       title: 'Blog Leaf',
       description: 'A Full-Featured Blog Posting Platform',
@@ -123,8 +35,8 @@ const Projects = () => {
       title: 'Netflix Clone',
       description: 'A replica of Netflix’s cover page, showcasing layout and design.',
       features: [
-        { title: 'UI/UX DESIGN & RESPONSIVE DESIGN', detail: 'Creating visually appealing, user-friendly interface that and implementing responsive design principles to ensure compatibility across various devices' },
-        // { title: 'RESPONSIVE DESIGN', detail: 'Implementing responsive design principles to ensure compatibility across various devices.' }
+        { title: 'UI/UX DESIGN & RESPONSIVE DESIGN', detail: 'Creating visually appealing, user-friendly interface that and implementing responsive design principles to ensure compatibility across various devices' }
+      
       ],
       techStack: ['HTML5', 'CSS3'],
       
